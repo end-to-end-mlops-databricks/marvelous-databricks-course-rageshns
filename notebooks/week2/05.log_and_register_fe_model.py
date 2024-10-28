0b1f344100c0a877fc7175e4eb41463aca3f1f13
Rageshns <rageshns@gmail.com>
2024-10-28 14:21:49 +0530

    build changes

--- notebooks/week2/05.log_and_register_fe_model.py
+++ notebooks/week2/05.log_and_register_fe_model.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
-# MAGIC %pip install /Volumes/mlops_students/rageshns/data/mlops_with_databricks-0.0.1-py3-none-any.whl
+# MAGIC %pip install mlops_with_databricks-0.0.1-py3-none-any.whl
 
 # COMMAND ----------
 
--- src/house_price/data_processor.py
+++ src/house_price/data_processor.py
@@ -1,50 +1,87 @@
+# import datetime
+from datetime import datetime
+
 import pandas as pd
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp, to_utc_timestamp
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from house_price.config import ProjectConfig
 
 
 class DataProcessor:
-    def __init__(self, filepath, config):
-        self.df = self.load_data(filepath)
-        self.config = config
-        self.X = None
-        self.y = None
-        self.preprocessor = None
+    def __init__(self, pandas_df: pd.DataFrame, config: ProjectConfig):
+        self.df = pandas_df  # Store the DataFrame as self.df
+        self.config = config  # Store the configuration
 
-    def load_data(self, filepath):
-        return pd.read_csv(filepath)
+    def preprocess(self):
+        """Preprocess the DataFrame stored in self.df"""
+        # Handle missing values and convert data types as needed
+        self.df["LotFrontage"] = pd.to_numeric(self.df["LotFrontage"], errors="coerce")
 
-    def preprocess_data(self):
-        # Remove rows with missing target
-        target = self.config["target"]
-        self.df = self.df.dropna(subset=[target])
+        self.df["GarageYrBlt"] = pd.to_numeric(self.df["GarageYrBlt"], errors="coerce")
+        median_year = self.df["GarageYrBlt"].median()
+        self.df["GarageYrBlt"].fillna(median_year, inplace=True)
+        current_year = datetime.now().year
 
-        # Separate features and target
-        self.X = self.df[self.config["num_features"] + self.config["cat_features"]]
-        self.y = self.df[target]
+        self.df["GarageAge"] = current_year - self.df["GarageYrBlt"]
+        self.df.drop(columns=["GarageYrBlt"], inplace=True)
 
-        # Create preprocessing steps for numeric and categorical data
-        numeric_transformer = Pipeline(
-            steps=[("imputer", SimpleImputer(strategy="median")), ("scaler", StandardScaler())]
+        # Handle numeric features
+        num_features = self.config.num_features
+        for col in num_features:
+            self.df[col] = pd.to_numeric(self.df[col], errors="coerce")
+
+        # Fill missing values with mean or default values
+        self.df.fillna(
+            {
+                "LotFrontage": self.df["LotFrontage"].mean(),
+                "MasVnrType": "None",
+                "MasVnrArea": 0,
+            },
+            inplace=True,
         )
 
-        categorical_transformer = Pipeline(
-            steps=[
-                ("imputer", SimpleImputer(strategy="constant", fill_value="missing")),
-                ("onehot", OneHotEncoder(handle_unknown="ignore")),
-            ]
-        )
+        # Convert categorical features to the appropriate type
+        cat_features = self.config.cat_features
+        for cat_col in cat_features:
+            self.df[cat_col] = self.df[cat_col].astype("category")
 
-        # Combine preprocessing steps
-        self.preprocessor = ColumnTransformer(
-            transformers=[
-                ("num", numeric_transformer, self.config["num_features"]),
-                ("cat", categorical_transformer, self.config["cat_features"]),
-            ]
-        )
+        # Extract target and relevant features
+        target = self.config.target
+        relevant_columns = cat_features + num_features + [target] + ["Id"]
+        self.df = self.df[relevant_columns]
 
     def split_data(self, test_size=0.2, random_state=42):
-        return train_test_split(self.X, self.y, test_size=test_size, random_state=random_state)
+        """Split the DataFrame (self.df) into training and test sets."""
+        train_set, test_set = train_test_split(self.df, test_size=test_size, random_state=random_state)
+        return train_set, test_set
+
+    def save_to_catalog(self, train_set: pd.DataFrame, test_set: pd.DataFrame, spark: SparkSession):
+        """Save the train and test sets into Databricks tables."""
+
+        train_set_with_timestamp = spark.createDataFrame(train_set).withColumn(
+            "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC")
+        )
+
+        test_set_with_timestamp = spark.createDataFrame(test_set).withColumn(
+            "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC")
+        )
+
+        train_set_with_timestamp.write.mode("append").saveAsTable(
+            f"{self.config.catalog_name}.{self.config.schema_name}.train_set"
+        )
+
+        test_set_with_timestamp.write.mode("append").saveAsTable(
+            f"{self.config.catalog_name}.{self.config.schema_name}.test_set"
+        )
+
+        spark.sql(
+            f"ALTER TABLE {self.config.catalog_name}.{self.config.schema_name}.train_set "
+            "SET TBLPROPERTIES (delta.enableChangeDataFeed = true);"
+        )
+
+        spark.sql(
+            f"ALTER TABLE {self.config.catalog_name}.{self.config.schema_name}.test_set "
+            "SET TBLPROPERTIES (delta.enableChangeDataFeed = true);"
+        )
