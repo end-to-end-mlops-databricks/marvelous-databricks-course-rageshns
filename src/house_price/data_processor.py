7a75215985bc46394d3cf34bb4cf2c0fc51c314c
Rageshns <rageshns@gmail.com>
2024-10-28 15:44:37 +0530

    build changes

--- src/house_price/data_processor.py
+++ src/house_price/data_processor.py
@@ -27,9 +27,9 @@ class DataProcessor:
         self.df["GarageAge"] = current_year - self.df["GarageYrBlt"]
         self.df.drop(columns=["GarageYrBlt"], inplace=True)
 
-        # remove for NaN
-        self.df.dropna(inplace=True)
-
+        # Fill missing values with mean or default values
+        self.df.fillna(0, inplace=True)
+        
         # Handle numeric features
         num_features = self.config.num_features
         for col in num_features:
