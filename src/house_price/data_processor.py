006fd8e4db712a5925bb54ecfd5890ca5642fe61
Rageshns <rageshns@gmail.com>
2024-10-28 16:13:45 +0530

    build changes

--- src/house_price/data_processor.py
+++ src/house_price/data_processor.py
@@ -30,6 +30,8 @@ class DataProcessor:
         # Fill missing values with mean or default values
         self.df.fillna(0, inplace=True)
 
+        self.df.dropna(inplace=True)
+
         # Handle numeric features
         num_features = self.config.num_features
         for col in num_features:
