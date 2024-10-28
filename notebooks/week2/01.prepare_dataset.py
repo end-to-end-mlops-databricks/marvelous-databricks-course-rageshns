c352f0d5f2bed2ca38fa1bc59e534cd8bbe16791
Rageshns <rageshns@gmail.com>
2024-10-28 14:31:16 +0530

    build changes

--- notebooks/week2/01.prepare_dataset.py
+++ notebooks/week2/01.prepare_dataset.py
@@ -1,6 +1,12 @@
 # Databricks notebook source
 # MAGIC %pip install mlops_with_databricks-0.0.1-py3-none-any.whl
 
+# COMMAND ----------
+
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
 from pyspark.sql import SparkSession
 
 from house_price.config import ProjectConfig
--- notebooks/week2/03.log_and_register_model.py
+++ notebooks/week2/03.log_and_register_model.py
@@ -1,5 +1,12 @@
 # Databricks notebook source
 # MAGIC %pip install mlops_with_databricks-0.0.1-py3-none-any.whl
+
+
+# COMMAND ----------
+
+dbutils.library.restartPython()
+
+# COMMAND ----------
 import mlflow
 from lightgbm import LGBMRegressor
 from mlflow.models import infer_signature
