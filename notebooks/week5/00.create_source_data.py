444c64a7c96035cd87c06cdb8b2e4b28bbee2430
Rageshns <rageshns@gmail.com>
2024-11-22 13:11:44 +0530

    build changes

--- notebooks/week5/00.create_source_data.py
+++ notebooks/week5/00.create_source_data.py
@@ -1,4 +1,10 @@
 # Databricks notebook source
+# MAGIC %pip install ../mlops_with_databricks-0.0.1-py3-none-any.whl
+
+# COMMAND ----------
+# MAGIC %restart_python
+
+# COMMAND ----------
 import numpy as np
 import pandas as pd
 from pyspark.sql import SparkSession
